664866cb4a6485e175776ba9acac983857c6d0c9
Deathtreak <beber76.lecoffre@orange.fr>
2020-04-07 12:05:36 +0200

    Rocket Ship base control : thrust , rotate

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+
+    void Start()
+    {
+        GetAllRequiredComponents();
+    }
+
+    private void GetAllRequiredComponents()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        if (!m_rigidbody)
+            Debug.LogError("Can't find rigidbody component on Player rocket !");
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Thrust
+            m_rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //Left
+            transform.Rotate(Vector3.forward * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //Right
+            transform.Rotate(Vector3.back * Time.deltaTime);
+        }
+    }
+}
