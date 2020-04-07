ad9a69092a92a38e1aa2afeb465f6c72b38cc725
Deathtreak <beber76.lecoffre@orange.fr>
2020-04-07 15:06:27 +0200

    Light Level Design

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,6 +2,11 @@
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField]
+    private float m_rcsThrust = 100f;
+    [SerializeField]
+    private float m_mainThrust = 1000f;
+
     private Rigidbody m_rigidbody;
     private AudioSource m_audioSource;
 
@@ -29,12 +34,19 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
+            float thrustForceThisFrame = m_mainThrust * Time.deltaTime;
             //Thrust
-            m_rigidbody.AddRelativeForce(Vector3.up);
-            
-            if(!m_audioSource.isPlaying)
+            m_rigidbody.AddRelativeForce(Vector3.up * thrustForceThisFrame);
+
+            if (!m_audioSource.isPlaying)
                 m_audioSource.Play();
         }
         else
@@ -42,16 +54,41 @@ public class Rocket : MonoBehaviour
             if (m_audioSource.isPlaying)
                 m_audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        m_rigidbody.freezeRotation = true; //take manual control of rotation
+
+        float rotationSpeed = m_rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Q))
         {
             //Left
-            transform.Rotate(Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             //Right
-            transform.Rotate(Vector3.back * Time.deltaTime);
+            transform.Rotate(Vector3.back * rotationSpeed);
+        }
+
+        m_rigidbody.freezeRotation = false; //resume physic's control of rotation
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly Collision");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel");
+                break;
+            default:
+                Debug.Log("Your dead");
+                break;
         }
     }
 }
