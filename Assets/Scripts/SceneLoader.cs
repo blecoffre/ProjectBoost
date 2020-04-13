f6bff07ff6e74eb08056e6453f0d43e3c0d65c45
Deathtreak <beber76.lecoffre@orange.fr>
2020-04-13 13:14:05 +0200

    Simple Loading Scene with progress bar

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_loadingBarContainer = default;
+    [SerializeField]
+    private Image m_loadingBarFill;
+    [SerializeField]
+
+    private AsyncOperation asyncLoadScene;
+
+    /// <summary>
+    /// Load scene async and open it after a certain amount of time
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <param name="withProgressBar"></param>
+    /// <param name="timeAfterOpen"></param>
+
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsynchronously(LevelManager.LevelName, LevelManager.WithProgressBar, LevelManager.TimeBeforeOpen));
+    }
+
+    private IEnumerator LoadAsynchronously(string levelName, bool withProgressBar, float timeBeforeOpen)
+    {
+        if (!withProgressBar)
+            m_loadingBarContainer?.SetActive(false);
+
+        float timer = 0f;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            if (m_loadingBarFill && withProgressBar)
+            {
+                float progress = Mathf.Clamp01(operation.progress / 0.9f);
+                m_loadingBarFill.fillAmount = progress;
+            }
+
+            timer += Time.deltaTime;
+
+
+            if (timer > timeBeforeOpen)
+            {
+                operation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+}
